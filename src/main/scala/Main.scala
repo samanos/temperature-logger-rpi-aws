89a3a407abd63d577d660a27fc31655f580af6c8
Martynas Mickevičius <mmartynas@gmail.com>
2016-03-28 19:11:47 +0300

    Complete reporting app

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,15 +3,11 @@ package io.github.samanos.tlog
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.marshalling.ToRequestMarshaller
+import akka.http.scaladsl.marshalling.Marshal
 import akka.http.scaladsl.model._
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl._
 import akka.util.ByteString
-
-import scala.concurrent.{ Await, Future }
-import scala.concurrent.duration._
-import scala.util.Try
 
 import spray.json._
 
@@ -29,26 +25,24 @@ object Main extends App {
 
   import sys.dispatcher
 
-  val p = implicitly[ToRequestMarshaller[Payload]]
-  //p.
-
   val interval = sys.settings.config.getDuration("tlog.interval").toScala
   val makerkey = sys.settings.config.getString("tlog.maker-key")
-  val report = Uri(sys.settings.config.getString("tlog.report-url"))
+  val reportTo = Uri(sys.settings.config.getString("tlog.report-url") + makerkey)
 
   val gpio = Gpio(sys.settings.config)
-  val pool = Http().cachedHostConnectionPool[Int](report.authority.host.toString)
+  val pool = Http().cachedHostConnectionPool[Int](reportTo.authority.host.toString)
 
   Source.tick(interval, interval, ())
     .map(_ => gpio.temperature.toList)
-    .map(t => Payload(t.headOption.getOrElse(0), t.tail.headOption.getOrElse(0)))
-    .map(p => HttpRequest(uri = report.path.toString + makerkey, entity = ???))
-
-
-  //val responseFuture: Future[(Try[HttpResponse], Int)] = Source.single() -> 42)
-  //  .via(pool)
-  //  .runWith(Sink.head)
-
-  //Await.ready(responseFuture, 10.seconds)
-
+    .mapConcat {
+      case value :: Nil => List(Payload(value, 0))
+      case value1 :: value2 :: Nil => List(Payload(value1, value2))
+      case value1 :: value2 :: rest => List(Payload(value1, value2))
+      case Nil => sys.log.warning("Unable to read temperature. Check if the module is connected properly."); Nil
+    }
+    .mapAsync(parallelism = 1)(p => Marshal((HttpMethods.POST, reportTo, p)).to[HttpRequest])
+    .map(_ -> 42)
+    .runForeach {
+      case (response, _) => response.entity.dataBytes.runWith(Sink.ignore)
+  }
 }
