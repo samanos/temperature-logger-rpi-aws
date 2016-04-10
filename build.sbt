a4c17effbc3e42577f4414263ef4bf41b68de14d
Martynas Mickevičius <mmartynas@gmail.com>
2016-04-10 17:47:02 +0300

    Read security creds from single line base64 strings

--- build.sbt
+++ build.sbt
@@ -12,7 +12,6 @@ libraryDependencies ++= Seq(
   "com.github.pathikrit" %% "better-files"                    % "2.15.0",
   "org.eclipse.paho"      % "org.eclipse.paho.client.mqttv3"  % "1.0.2",
   "org.bouncycastle"      % "bcprov-jdk15on"                  % "1.54",
-  "org.bouncycastle"      % "bcpkix-jdk15on"                  % "1.54",
   "com.typesafe.akka"    %% "akka-testkit"                    % Akka      % Test,
   "com.typesafe.akka"    %% "akka-stream-testkit"             % Akka      % Test,
   "org.scalatest"        %% "scalatest"                       % "2.2.6"   % Test,
--- src/main/scala/Mqtt.scala
+++ src/main/scala/Mqtt.scala
@@ -1,26 +1,22 @@
 package io.github.samanos.tlog
 
 import akka.Done
+import akka.stream.scaladsl.Flow
+
+import com.typesafe.config.Config
 
 import org.eclipse.paho.client.mqttv3._
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider
-import org.bouncycastle.openssl.PEMParser
-import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter
-import org.bouncycastle.cert.X509CertificateHolder
 
-import java.io.StringReader
-import java.security.Security
-import org.bouncycastle.openssl.PEMKeyPair
-import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter
-import java.security.KeyStore
-import javax.net.ssl.TrustManagerFactory
-import javax.net.ssl.KeyManagerFactory
-import javax.net.ssl.SSLContext
-import com.typesafe.config.ConfigFactory
-import com.typesafe.config.Config
-import akka.stream.scaladsl.Flow
+import java.io.ByteArrayInputStream
+import java.security._
+import java.security.cert._
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.interfaces.RSAPrivateKey
+import java.util.Base64
+import javax.net.ssl._
 
 object Mqtt {
 
@@ -44,34 +40,31 @@ object Mqtt {
   }
 
   /**
-   * Code humbly borrowed from:
+   * Code humbly borrowed and adapted from:
    * https://github.com/awslabs/aws-iot-demo-for-danbo/blob/master/danbo-pi-client/src/main/java/com/amazonaws/services/iot/demo/danbo/rpi/SslUtil.java
    */
   def getSslSocketFactory(caCrt: String, clCrt: String, prKey: String) = {
     Security.addProvider(new BouncyCastleProvider());
 
-    // load CA certificate
-    val caCrtParser = new PEMParser(new StringReader(caCrt))
-    val caCert = caCrtParser.readObject().asInstanceOf[X509CertificateHolder]
-    caCrtParser.close
+    val caCert = CertificateFactory
+      .getInstance("X.509")
+      .generateCertificate(new ByteArrayInputStream(Base64.getDecoder.decode(caCrt)))
+      .asInstanceOf[X509Certificate]
 
-    // load client certificate
-    val clCrtParser = new PEMParser(new StringReader(clCrt))
-    val clCert = clCrtParser.readObject().asInstanceOf[X509CertificateHolder]
-    clCrtParser.close
+    val clCert = CertificateFactory
+      .getInstance("X.509")
+      .generateCertificate(new ByteArrayInputStream(Base64.getDecoder.decode(clCrt)))
+      .asInstanceOf[X509Certificate]
 
-    // load client private key
-    val prKeyParser = new PEMParser(new StringReader(prKey))
-    val privKey = (new JcaPEMKeyConverter().setProvider("BC")).getKeyPair(prKeyParser.readObject().asInstanceOf[PEMKeyPair])
-    prKeyParser.close
-
-    val certConverter = new JcaX509CertificateConverter()
-    certConverter.setProvider("BC")
+    val privKey = KeyFactory
+      .getInstance("RSA")
+      .generatePrivate(new PKCS8EncodedKeySpec(Base64.getDecoder.decode(prKey)))
+      .asInstanceOf[RSAPrivateKey]
 
 		// CA certificate is used to authenticate server
     val caKs = KeyStore.getInstance(KeyStore.getDefaultType())
     caKs.load(null, null)
-    caKs.setCertificateEntry("ca-certificate", certConverter.getCertificate(caCert))
+    caKs.setCertificateEntry("ca-certificate", caCert)
 
     val tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
     tmf.init(caKs)
@@ -79,8 +72,8 @@ object Mqtt {
     // Client key and certificates are sent to server so it can authenticate us
 		val ks = KeyStore.getInstance(KeyStore.getDefaultType())
     ks.load(null, null);
-    ks.setCertificateEntry("certificate",	certConverter.getCertificate(clCert))
-    ks.setKeyEntry("private-key", privKey.getPrivate(), "".toCharArray(), Array(certConverter.getCertificate(clCert)))
+    ks.setCertificateEntry("certificate",	clCert)
+    ks.setKeyEntry("private-key", privKey, "".toCharArray(), Array(clCert))
 
     val kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm())
     kmf.init(ks, "".toCharArray());
